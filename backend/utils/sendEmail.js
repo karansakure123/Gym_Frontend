876816a8aa43b3dd53277c31d6c6c8a1e02ff225
Laxman Sakure <laxmansakure007@gmail.com>
2024-06-01 09:29:47 +0530

    Initial commit

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,31 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './Config.env' });
+
+export const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // use TLS
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: `${options.message} \n\n Email of user who sent the message: ${options.userEmail}`,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+  } catch (error) {
+    console.error("Error sending email: ", error);
+    throw error;
+  }
+};
